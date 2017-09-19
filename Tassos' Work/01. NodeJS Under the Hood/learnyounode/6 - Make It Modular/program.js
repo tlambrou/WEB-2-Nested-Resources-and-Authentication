55abcf621edfb169691e983e0f88680c410c0857
tlambrou <lambrou@gmail.com>
2017-09-19 15:43:48 -0700

    Finished Exercise 6

--- Tassos' Work/01. NodeJS Under the Hood/learnyounode/6 - Make It Modular/program.js
+++ Tassos' Work/01. NodeJS Under the Hood/learnyounode/6 - Make It Modular/program.js
@@ -1,8 +1,6 @@
 var fs = require('fs')
-var path = require('path')
 var tassos = require('./tassos_module')
 
-var result = []
 const dirname = process.argv[2]
 const extension = process.argv[3]
 
@@ -10,11 +8,12 @@ let callback = (err, data) => {
   if (err) {
     return console.error("Whoops there was an error!\nError: " + err)
   } else {
-    result = data.filter((n) => {return (path.extname(n) == '.' + extension)})
-    for (var i = 0; i < result.length; i++){
-      console.log(result[i])
-    }
+
+    data.map((n) => {
+      return console.log(n)
+    })
+
   }
 }
 
-tassos.tassos_module(dirname, extension, callback)
+tassos(dirname, extension, callback)
--- Tassos' Work/01. NodeJS Under the Hood/learnyounode/6 - Make It Modular/tassos_module.js
+++ Tassos' Work/01. NodeJS Under the Hood/learnyounode/6 - Make It Modular/tassos_module.js
@@ -1,13 +1,30 @@
 var fs = require('fs')
+var path = require('path')
 
-module.exports.tassos_module = (dirname, extension, callback) => {
+var result = []
+module.exports = (dirname, extension, callback) => {
 
   fs.readdir(dirname, (err, data) => {
     if (err) {
       return callback(err)
     } else {
-      return callback(null, data)
+
+      result = data.filter((n) => {
+
+        return (path.extname(n) == '.' + extension)
+      })
+
+      // console.log("the array is: " +typeof dataArr + " ....." + dataArr)
+      callback(null, result)
+
     }
   })
+
+  // for (var i = 0; i < thing.length; i++){
+  //   console.log(thing[i])
+  // }
+
+  // console.log("This is the thing:", thing)
+  return result
 
 }
