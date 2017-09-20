2cd71916b86ea9e837704809ed7e992522e70b2c
tlambrou <lambrou@gmail.com>
2017-09-20 15:40:21 -0700

    Completed tutorial 8

---
+++ Tassos' Work/learnyounode/8 - HTTP Collect/program.js
@@ -0,0 +1,16 @@
+var httpModule = require('http')
+var bl = require('bl')
+var concat = require('concat-stream')
+
+const url = process.argv[2]
+
+httpModule.get(url, (response) => {
+  response.setEncoding('utf8')
+  response.pipe(
+    bl((err, data) => {
+      console.log(data.length)
+      console.log(data.toString('utf8'))
+    }))
+}).on('error', (error) => {
+  console.log("Got error: " + error.message)
+})
